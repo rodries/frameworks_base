b7f81cf09c3b53123743184aff494d79477e1654
nebkat <nebkat@teamhacksung.org>
2012-02-17 12:55:02 +0000

    NotificationManagerService: Fix notification led bug
    We can assume that if the user turned the screen off while there was
    still an active notification then they wanted to keep the notification
    for later. In this case we shouldn't flash the notification light.
    
    The problem for some special notifications (such as missed calls)
    is that the screen is automatically turned on, and then when it is
    turned back off the led does not get enabled. For these notifications
    we use a flag to force the notification light even if the screen was
    turned off.
    
    Conflicts:
    
    	services/java/com/android/server/NotificationManagerService.java

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -109,6 +109,7 @@ public class NotificationManagerService extends INotificationManager.Stub
 
     // for enabling and disabling notification pulse behavior
     private boolean mScreenOn = true;
+    private boolean mWasScreenOn = false;
     private boolean mInCall = false;
     private boolean mNotificationPulseEnabled;
 
@@ -382,6 +383,8 @@ public class NotificationManagerService extends INotificationManager.Stub
                 mScreenOn = true;
             } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
                 mScreenOn = false;
+                mWasScreenOn = true;
+                updateLightsLocked();
             } else if (action.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
                 mInCall = (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(
                         TelephonyManager.EXTRA_STATE_OFFHOOK));
@@ -1166,10 +1169,30 @@ public class NotificationManagerService extends INotificationManager.Stub
             }
         }
 
+        boolean wasScreenOn = mWasScreenOn;
+        mWasScreenOn = false;
+
+        if (mLedNotification == null) {
+            mNotificationLight.turnOff();
+            return;
+        }
+
+        // We can assume that if the user turned the screen off while there was
+        // still an active notification then they wanted to keep the notification
+        // for later. In this case we shouldn't flash the notification light.
+        // For special notifications that automatically turn the screen on (such
+        // as missed calls), we use this flag to force the notification light
+        // even if the screen was turned off.
+        boolean forceWithScreenOff = (mLedNotification.notification.flags &
+                Notification.FLAG_FORCE_LED_SCREEN_OFF) != 0;
+
         // Don't flash while we are in a call or screen is on
 				// Depending on ROMControl "Screen ON flash" flag
         if (mLedNotification == null || mInCall || (mScreenOn && !ledScreenOn)
-                || (inQuietHours() && mQuietHoursDim)) {
+                || (inQuietHours() && mQuietHoursDim)  || (wasScreenOn && !forceWithScreenOff)) {
+//=======
+//        if (mInCall || mScreenOn || (wasScreenOn && !forceWithScreenOff)) {
+//>>>>>>> 5713307... NotificationManagerService: Fix notification led bug
             mNotificationLight.turnOff();
         } else {
             int ledARGB = mLedNotification.notification.ledARGB;
